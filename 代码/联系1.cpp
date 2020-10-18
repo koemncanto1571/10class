d100a4ab997f312151d2bd6cd91787304e20d2a8
komencanto1571 <1571316921@qq.com>
2020-10-18 10:07:31 +0800

    练习

---
+++ 代码/联系1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello world\n");
+	printf("int 整型\n");
+	printf("short 短\n");
+	printf("long 长\n");
+	printf("scanf 扫描\n");
+	printf("float 单精度浮点\n");
+	printf("double 双精度浮点\n");
+	printf("getchar 输入字符\n");
+	printf("putchar 输出字符\n");
+	printf("----------------------\n"); 
+	 
+}
+ 
